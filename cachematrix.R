f01d36efccf3c1cec363b1c6c83998285254ac2b
Jonathan Stanger <arkore@users.noreply.github.com>
2014-09-17 14:32:02 +1000

    Adding some notes to work from

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## ERROR TRAPPING
+## Since the problem description states to assume matrix is always invertable
+## no testing of the input matrix for validity will be done.
+
+
+## This function creates an object with methods to cache the value of a matrix
+## and its calculated inverse to prevent the need to recalculate unless the matrix
+## changes
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## Needs 4 functions, set, get, setinverse, getinverse
+        ## Should check if the matrix is new, if not retain the old cache of
+        ## the inverse for extra computational efficientcy i.e. repeated
+        ## calls setting the matrix from the output of another function
 
 }
 
 
-## Write a short comment describing this function
+## This function will return the inverse of a matrix cached in the makeCacheMatrix
+## function or trigger the calculation if not already done
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Get the inverse using the method $getinverse
+        ## Check if inverse exists
+                ## If inverse exists then output
+        ## If no inverse exists obtain raw matrix
+        ## Caculate inverse of matrix
+        ## Store inverse in cache and return the inverse
 }
